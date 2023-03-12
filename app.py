6dc4943117bb2f603bd83a5c25139004079c796d
Md Saikat <darkhtml2275@gmail.com>
2023-03-12 16:18:13 +0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask('__name__')
 
 @app.route('/')
 def index():
-    return '// Poc By N00B', 200
+    return '// Poc By test N00B //11-03-2023 ', 200
  
 if (__name__) == "__main__":
     app.run()
